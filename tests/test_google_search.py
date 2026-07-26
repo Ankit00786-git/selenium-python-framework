3f7491a6723930e8f210aba0679cab07d90f1756
Ankit Pal <prac00786@gmail.com>
2026-07-26 05:46:08 +0000

    Add BasePage, Page Object Model and pytest fixture

--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -1,11 +1,8 @@
-from utils.driver_factory import DriverFactory
 from pages.google_page import GooglePage
 from config.config_reader import ConfigReader
 
 
-def test_google_search():
-
-    driver = DriverFactory.get_driver()
+def test_google_search(driver):
 
     page = GooglePage(driver)
 
@@ -14,5 +11,3 @@ def test_google_search():
     page.search("Selenium Python")
 
     assert "Google" in page.get_title()
-
-    page.quit()
