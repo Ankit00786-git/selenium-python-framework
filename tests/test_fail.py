065744acbf63dbaabe1cda6e4e08c0f5b11ae166
Ankit00786-git <prac00786@gmail.com>
2026-07-31 18:00:53 +0000

    commit to add webhook

--- tests/test_fail.py
+++ tests/test_fail.py
@@ -2,6 +2,7 @@ from pages.google_page import GooglePage
 from config.config_reader import ConfigReader
 
 
+'''alive is awesome first webhook trigger'''
 def test_failure(driver):
 
     page = GooglePage(driver)
